38470c8397dc03e6a8d148752b30334fe12099a0
VicBilibily <mail@oyml.cn>
2020-01-12 17:26:57 +0800

    :white_check_mark: 项目测试

---
+++ test/OYMLCN.AspNetCore.Test/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OYMLCN.Extensions;
+using OYMLCN.Helpers;
+
+namespace OYMLCN.AspNetCore.Test.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello World!");
+        }
+
+
+        public class UserInfo : IUserInfo
+        {
+            public string ID { get; set; }
+            public string Name { get; set; }
+        }
+        public IActionResult Login()
+        {
+            var jwt = this.BuildJwtSecurityToken(new UserInfo()
+            {
+                ID = StringHelper.RandCode(onlyNumber: true),
+                Name = StringHelper.RandBlurCode(20)
+            });
+            this.SignInJwt(jwt);
+            return (JsonResult)jwt;
+        }
+
+        public IActionResult Refresh(string refresh_token)
+        {
+            var jwt = this.RefreshJwtSecurityToken(refresh_token);
+            if (jwt == null)
+                return Content("refresh_token超时或已使用");
+            this.SignInJwt(jwt);
+            return (JsonResult)jwt;
+        }
+
+        [Authorize]
+        public IActionResult TestAuth()
+        {
+            var info = this.GetUserInfo<UserInfo>();
+            return Content($"验证通过，身份信息：{info.ToJsonString()}");
+        }
+    }
+}
---
+++ test/OYMLCN.AspNetCore.Test/Startup.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Logging;
+
+namespace OYMLCN.AspNetCore.Test
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddJwtAuthentication();
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            // 用于调试部分异常以显示敏感信息，正式项目不要出现此句
+            IdentityModelEventSource.ShowPII = true; 
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
